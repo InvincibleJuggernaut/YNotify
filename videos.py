31820805d1541d5ab77c5ff5009730913716a19e
Manikandan Nair <mani4pmn@gmail.com>
2020-12-12 06:51:20 +0000

    Add files via upload

--- videos.py
+++ videos.py
@@ -1,23 +1,18 @@
 import urllib3
 import json
 
-API_KEY = ''
-channel_id=''
-
-url='https://www.googleapis.com/youtube/v3/search?key='+API_KEY+'&channelId='+channel_id+'&part=snippet,id&order=date&maxResults=10'
-
-http=urllib3.PoolManager()
-r=http.request('GET',url)
-json_response=json.loads(r.data.decode('utf-8'))
-
-try:
-    response_results=json_response['items']
-    for x in response_results:
-        print(x['snippet']['title'])
-        print(x['snippet']['description'])
-        print(x['id']['videoId'])
-        print(x['snippet']['thumbnails']['default']['url'])
-        print()
-except:
-    print('Invalid request')
-
+def videos(channel_id, API_KEY):
+    url='https://www.googleapis.com/youtube/v3/search?key='+API_KEY+'&channelId='+channel_id+'&part=snippet,id&order=date&maxResults=10'
+    http=urllib3.PoolManager()
+    r=http.request('GET',url)
+    json_response=json.loads(r.data.decode('utf-8'))
+    try:
+        response_results=json_response['items']
+        for x in response_results:
+            print(x['snippet']['title'])
+            print(x['snippet']['description'])
+            print(x['id']['videoId'])
+            print(x['snippet']['thumbnails']['default']['url'])
+            print()
+    except:
+        print('Invalid request')
