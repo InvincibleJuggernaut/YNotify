04d757dbf25f6570b0c4ef6de7b12a3d2051ee64
Manikandan Nair <mani4pmn@gmail.com>
2020-12-15 08:38:20 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask, render_template
+from datetime import date, timedelta
 
 app=Flask(__name__)
 
 from videos import videos
 from read import read
+from credentials import connection_url
+from credentials import API_KEY
+from actions import find, insert
 
 @app.route("/")
 def index():
-    
-    list_of_id=read()
-    titles, description, video_id, thumbnails, published_at, url=videos(list_of_id, API_KEY)
-    '''
-    print(titles)
-    print(description)
-    print(video_id)
-    print(thumbnails)
-    print(published_at)
-    '''
-    return render_template("index.html",n=len(titles), titles=titles, description=description, video_id=video_id, thumbnails=thumbnails, published_at=published_at, url=url)
+    today=(date.today()).strftime("%Y-%m-%d")
+    result=find('Date',today)
+    if(result is None):
+        print('Need to invoke API')
+        list_of_id=read()
+        titles, description, video_id, thumbnails, published_at, url=videos(list_of_id, API_KEY)
+        insert(today, titles, description, video_id, thumbnails, published_at, url)
+        return render_template("index.html",n=len(titles), titles=titles, description=description, video_id=video_id, thumbnails=thumbnails, published_at=published_at, url=url)
+    else:
+        print('Data already present')
+        return render_template("index.html",n=len(result['Title']), titles=result['Title'], description=result['Desc'], video_id=result['V_ID'], thumbnails=result['Thumb'], published_at=result['Pub'], url=result['URL'])
 
 if __name__=="__main__":
     app.run(debug=True)
